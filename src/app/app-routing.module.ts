00f15005b91b9b05dacd178a30e1b33cd9a50518
Raddedi Ala <alaradadi22@gmail.com>
2023-02-25 16:06:55 +0100

    commit 1

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { PatientComponent } from './component/patient/patient.component';
+import { AccueilComponent } from './component/accueil/accueil.component';
+import { AboutUsComponent } from './component/about-us/about-us.component';
+import { IndexComponent } from './component/index/index.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+
+const routes: Routes = [
+  {path:"",component:IndexComponent},
+  {path:"login",component:LoginComponent},
+  {path:"about-us",component:AboutUsComponent},
+  {path:"accueil",component:AccueilComponent},
+  {path:"patient",component:PatientComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IndexComponent } from './component/index/index.component';
+import { LoginComponent } from './component/login/login.component';
+import { AboutUsComponent } from './component/about-us/about-us.component';
+import { AccueilComponent } from './component/accueil/accueil.component';
+import { PatientComponent } from './component/patient/patient.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    LoginComponent,
+    AboutUsComponent,
+    AccueilComponent,
+    PatientComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
